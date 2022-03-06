f4240e8005d792f925e5ee032fff1df296aaf166
shimaes <97337477+shimaes@users.noreply.github.com>
2022-03-06 16:54:54 +0330

    commit quiz oop

---
+++ questions.js
@@ -0,0 +1,90 @@
+var questions = [
+{
+	"question": "[ده تقسیم بردو]?",
+	"option1" : "4",
+	"option2": "5",
+	"option3": "1",
+	"option4": "3",
+	"answer": 2
+}, 
+{
+	"question": "جذر81?",
+	"option1" : "5",
+	"option2": "9",
+	"option3": "4",
+	"option4": "2",
+	"answer": 2
+},
+{
+	"question": "زبان برنامه نویسی تحت وب ",
+	"option1" : "CSS",
+	"option2": "HTML",
+	"option3": "Javascript",
+	"option4": "Bootstrap",
+	"answer": 3
+},
+{
+	"question": "درمان هر درد?",
+	"option1" : "ژلوفن",
+	"option2": "تراپیست",
+	"option3": "ضدیخ",
+	"option4": "کرونا",
+	"answer": 1
+},
+{
+	"question": "ایزوتوپ خطرناک ید", 
+	"option1": "ندارد",
+	"option2": "235ید",
+	"option3": "129ید",
+	"option4": "131ید",
+	"answer": 4
+}, 
+{
+	"question": "بزرگترین حادثه هسته ای جهان?", 
+	"option1": "چرنوبیل",
+	"option2": "زلزله ژاپن",
+	"option3": "هیروشیما",
+	"option4": "ناکازاکی",
+	"answer": 1
+}, 
+{
+	"question": "پدر دوج کوین?", 
+	"option1": "استیو جابز",
+	"option2": "عموباب",
+	"option3": "ایلان ماسکا",
+	"option4": "تسلا",
+	"answer": 3
+}, 
+{
+	"question": "عمل جنگ اوکراین وروسیه?", 
+	"option1": "اوکراین",
+	"option2": "روسیه",
+	"option3": "ناتو",
+	"option4": "ژنو",
+	"answer": 3
+}, 
+{
+	"question": "نویسنده کتاب انسان خردمند?", 
+	"option1": "چامسکی",
+	"option2": "جوجو مویز",
+	"option3": "جنیفرلورنس",
+	"option4": "حراری ز",
+	"answer": 4
+}, 
+{
+	"question": "سال همه گیری کرونا ", 
+	"option1": "2020",
+	"option2": "2018l",
+	"option3": "2010",
+	"option4": "2019",
+	"answer": 4
+}, 
+{
+	"question": "نویسنده کتاب هنر برنامه نویسی ", 
+	"option1": "جی جی حدید",
+	"option2": "چامسکی",
+	"option3": "عمو باب",
+	"option4": "لینوکس",
+	"answer": 2
+}		
+];
